bcf713df9599e27456640db37c4ca5a4b83119c5
Neeraj Mittal <mittal.neeraj@gmail.com>
2022-10-07 00:33:03 +0530

    feature/utils yq (#5)
    ya install

--- test/utils/all_cli_versions.sh
+++ test/utils/all_cli_versions.sh
@@ -6,8 +6,6 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "check for helmfile" helmfile --version
-check "check for kind" kind --version
 check "check for yq" yq --version
 
 # Report result
