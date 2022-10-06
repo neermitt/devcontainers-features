83736e0d3f61bbb1cb5ca4c6dbcaa7b94d028c14
Neeraj Mittal <mittal.neeraj@gmail.com>
2022-10-06 18:48:20 +0000

    global tests

--- test/_global/all_the_clis.sh
+++ test/_global/all_the_clis.sh
@@ -8,6 +8,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "check for helmfile" helmfile --version
 check "check for kind" kind --version
+check "check for yq" yq --version
 
 # Report result
 reportResults
